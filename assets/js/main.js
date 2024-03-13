2a2dd625be7a09a7a715a105d030bdbe6e2a0c37
BlueButterBaby <bluebutterbaby@hotmail.com>
2024-03-13 16:57:10 +0100

    commit6

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,20 +24,6 @@
             $('.nav-bar').removeClass('sticky-top');
         }
     });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
 
 
     // Header carousel
